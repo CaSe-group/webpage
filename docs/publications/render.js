6e20c9c9eaca45e1b2a9c1fa1dbf4feae2533c36
Christian Brandt <35726766+replikation@users.noreply.github.com>
2022-08-10 12:11:56 +0200

    changing to badges instead of circles

--- docs/publications/render.js
+++ docs/publications/render.js
@@ -52,13 +52,13 @@ function handle_entry_object(d, container) {
         //<div class='altmetric-embed' data-badge-type='donut' data- doi="10.1038/nature.2012.9872"></div>
         
         if ("altmetric" in d) {
-            containerd3.append("div").classed ("data-badge-popover", "right").attr ("data-badge-type", "donut").attr ("data-doi", d["altmetric"]).attr ("data-hide-no-mentions", "true"); 
+            containerd3.append("div").classed ("data-badge-popover", "left").attr ("data-badge-type", "1").attr ("data-doi", d["altmetric"]).attr ("data-hide-no-mentions", "true"); 
         }
         
         // <span class="__dimensions_badge_embed__" data-doi="10.1001/jama.2016.9797"></span><script async src="https://badge.dimensions.ai/badge.js" charset="utf-8"></script>
         
         if ("dimensions" in d) {
-             containerd3.append("span").classed ("__dimensions_badge_embed__", true).attr ("data-doi", d["dimensions"]).attr ("data-style","small_circle");
+             containerd3.append("span").classed ("__dimensions_badge_embed__", true).attr ("data-doi", d["dimensions"]).attr ("data-style","large_rectangle");
              containerd3.append("script").attr ("async","false").attr("src","https://badge.dimensions.ai/badge.js");       
         }
         
@@ -211,12 +211,7 @@ function extract_bibtex_record(highlight, record) {
         render_me.push("");
     }
     if (record["DOI"]) {
-        render_me.push ({"dimensions": record["DOI"]});
-    } else {
-        render_me.push("");    
-    }
-    if (record["DOI"]) {
-        render_me.push ({"altmetric": record["DOI"]});
+        render_me.push ({"altmetric": record["DOI"], "dimensions" : record["DOI"]});
     } else {
         render_me.push("");    
     }
